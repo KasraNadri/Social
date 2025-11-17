43d2ba9c1a3d04dea81de1d887861de8796c7aeb
KasraNadri <kasra.n2018@gmail.com>
2025-11-17 08:11:05 +0330

    installed chi

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 type application struct {
 	config config
@@ -12,12 +16,25 @@ type config struct {
 	addr string
 }
 
-func (app *application) run() error {
+func (app *application) mount() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /health", app.healthCheckHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+
 	srv := &http.Server{
-		Addr:    app.config.addr,
-		Handler: mux,
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
 	}
+
+	log.Printf("Server has started at %s", app.config.addr)
+
 	return srv.ListenAndServe()
 }
